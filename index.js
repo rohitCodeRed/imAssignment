1463f5102a2439e08af1035cb08c3e1ac395ef54
rohitCodeRed <rohit06051993@gmail.com>
2021-10-10 02:17:09 +0530

    Cpu tracking for linux is completed. with auto restart

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 const express = require('express');
 const app = express();
-//const stringify = require('json-stringify');
-//var router = express.Router();
-//const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const si = require('systeminformation');
 const config = require('./config.js');
 const geneateDynamicHtml = require('./app/generate_html');
+const trackCpu = require('./app/platformTimer');
+const restartEvent = require('./app/restartEvent');
+
+
 
 const port = process.env.PORT || 3001;
+const DELAY = 1000;
 
+let platform = process.platform;
+let timerId = "";
 
-
-
-//app.use(express.static(__dirname + '/public'));
+ 
+switch(platform) {
+    case 'linux': console.log("Linux Platform");
+        timerId = setInterval(trackCpu.linuxOs, DELAY,{pid:process.pid});
+        break;
+    case 'win32': console.log("windows platform");
+        timerId = setInterval(trackCpu.windowOs, DELAY,{pid:process.pid});
+        break;   
+    default: console.log("unknown platform");
+}
 
 //setting a port
 app.set(port);
@@ -30,6 +41,7 @@ app.use(function (req, res, next) {
 // app.use('/api/user', require('./app/api/user_api'));
 // app.use('/api/logged', require('./app/api/logged_user_api'));
 // app.use('/api/apiAcess',require('./app/api/api_acess'));
+
 
 app.use('/', function(req, res) {
    //load the single view file (angular will handle the page changes on the front-end)
@@ -60,3 +72,42 @@ mongoose.connect(config.mongoDB_url,{
 (result) => { console.log("\n**-----------MongoDB connection established------------**\n DB:",result.name,"\n Host:",result.host,"\n Port:",result.port); },
 err => { console.log("Mongo DB connection error: ",JSON.stringify(err.message)); }
 );
+
+
+
+
+
+//Track each function cpu consumption...
+/* 
+const inspector = require('inspector');
+const session = new inspector.Session();
+session.connect();
+
+session.post('Profiler.enable', () => {
+
+  app.use('/', function(req, res) {
+    session.post('Profiler.start', () => {
+      //load the single view file (angular will handle the page changes on the front-end)
+      si.getStaticData().then(function(data){
+        //console.log(" info: ",data);
+        res.send( geneateDynamicHtml(data));
+        session.post('Profiler.stop', (err, { profile }) => {
+          // Write profile to disk, upload, etc.
+          if (!err) {
+            console.log("Performance..:", profile.endTime-profile.startTime);
+          }
+        });
+      })
+      .catch(function(error){
+        console.error(error)
+      });
+
+    });
+    
+ 
+   //res.sendFile("public/home.html",{root: __dirname });
+ });
+ 
+ */
+
+
